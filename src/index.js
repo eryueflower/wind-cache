8dec7a9d6162e78e12ab7e97b943606868f1d3a8
lijiefeng <853524950@qq.com>
2020-03-24 18:34:16 +0800

    v0.0.4

--- src/index.js
+++ src/index.js
@@ -16,6 +16,13 @@ class windCache {
     this.list = {}
     return true
   }
+
+  setSync(key, value) {
+    setTimeout(() => {
+      this.list[key] = value
+      return true
+    }, 0)
+  }
 }
 window.windCache = windCache
 export default windCache
